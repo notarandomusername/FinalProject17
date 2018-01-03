92e3feb8da990486ac50d4d35429b445b3fb5147
Unknown <notanotherrandomusername@gmail.com>
2018-01-02 21:57:55 -0500

    Added Bootstrap and made a navbar.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
+    <navbar/>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar';
 export default {
   name: 'app',
+  components: { Navbar },
 };
 </script>
 
 <style>
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -20,4 +24,5 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-default navbar-fixed-top">
+      <a class="navbar-brand">
+        <i class="glyphicon glyphicon-tasks"></i>
+        Portfolio
+      </a>
+      <ul class="nav navbar-nav">
+        <li><router-link to="/">My Works</router-link></li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+nav {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+</style>
